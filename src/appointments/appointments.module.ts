4e1737208b9f673a91f82071cde8b009ca8a539e
Abdelaziz debbabi <wassimdebbabi12cc@gmail.com>
2026-07-12 20:37:59 +0100

    Update appointments.module.ts

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppointmentsService } from './appointments.service';
+import { PrismaService } from '../prisma.service';
 import { AppointmentsController } from './appointments.controller';
+import { AppointmentsService } from './appointments.service';
 
 @Module({
   controllers: [AppointmentsController],
-  providers: [AppointmentsService],
+  providers: [AppointmentsService, PrismaService],
 })
 export class AppointmentsModule {}

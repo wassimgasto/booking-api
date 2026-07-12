5ab1c21e7950f48ac98f535602e8a25b9852b121
Abdelaziz debbabi <wassimdebbabi12cc@gmail.com>
2026-07-12 16:51:01 +0000

    chore: clean repo

--- dist/appointments/dto/update-appointment.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateAppointmentDto } from './create-appointment.dto';
-declare const UpdateAppointmentDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateAppointmentDto>>;
-export declare class UpdateAppointmentDto extends UpdateAppointmentDto_base {
-}
-export {};
